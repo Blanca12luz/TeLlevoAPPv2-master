eab3eb33eba06ef8c75d52e5adaf3da91020228a
Blanca12luz <bl.hidalgo@duocuc.cl>
2024-11-30 16:46:59 -0300

    pagina notificaciones agregada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -30,7 +30,11 @@ const routes: Routes = [
   {
     path: 'historial',
     loadChildren: () => import('./pages/historial/historial.module').then( m => m.HistorialPageModule)
+  },  {
+    path: 'notificaciones',
+    loadChildren: () => import('./pages/notificaciones/notificaciones.module').then( m => m.NotificacionesPageModule)
   },
+
 
 ];
 

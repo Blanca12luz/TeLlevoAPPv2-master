2cf84acb71d79aaac544a01a1b1d189b0fc5f275
Blanca12luz <bl.hidalgo@duocuc.cl>
2024-11-30 16:28:44 -0300

    solicitar viaje vista lista (falta agregar la opcion de que escojer )

--- src/app/pages/solicitar-viaje/solicitar-viaje.page.ts
+++ src/app/pages/solicitar-viaje/solicitar-viaje.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-solicitar-viaje',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./solicitar-viaje.page.scss'],
 })
 export class SolicitarViajePage implements OnInit {
+  public viajes: any[] = []; // Lista de viajes obtenidos desde Firebase
 
-  constructor() { }
+  constructor(private firestore: AngularFirestore) {}
 
   ngOnInit() {
+    this.obtenerViajes();
   }
 
+  /**
+   * Método para obtener los viajes desde Firebase
+   */
+  obtenerViajes() {
+    this.firestore
+      .collection('viajes') // Nombre de la colección en Firebase
+      .valueChanges({ idField: 'id' }) // Recuperar datos con ID
+      .subscribe(
+        (data: any[]) => {
+          this.viajes = data;
+          console.log('Viajes obtenidos:', this.viajes);
+        },
+        (error) => {
+          console.error('Error al obtener viajes:', error);
+        }
+      );
+  }
 }

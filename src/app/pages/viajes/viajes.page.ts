f88940c125b9e5455532061d9e0cfbd679277081
Blanca12luz <bl.hidalgo@duocuc.cl>
2024-12-11 23:28:17 -0300

    borrar viaje creado

--- src/app/pages/viajes/viajes.page.ts
+++ src/app/pages/viajes/viajes.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore,  } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-viajes',
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./viajes.page.scss'],
 })
 export class ViajesPage implements OnInit {
+  public viajes: any[] = []; // Lista de viajes obtenidos desde Firebase
 
-  constructor() { }
+  constructor(private firestore: AngularFirestore) {}
 
   ngOnInit() {
+    this.obtenerViajes();
   }
 
+  /**
+   * Método para obtener los viajes desde Firebase
+   */
+  obtenerViajes() {
+    this.firestore
+      .collection('viajes') // Nombre de la colección en Firebase
+      .valueChanges({ idField: 'id' }) // Recuperar datos con ID
+      .subscribe(
+        (data: any[]) => {
+          this.viajes = data;
+          console.log('Viajes obtenidos:', this.viajes);
+        },
+        (error) => {
+          console.error('Error al obtener viajes:', error);
+        }
+      );
+  }
+
+  async borrarviaje(viaje: any){
+    await deleteDoc(this.viajes,viaje);
+    console.log("Viaje eliminado");
+  }
 }
+function deleteDoc(viajes: any[], viaje: any) {
+  throw new Error('Function not implemented.');
+}
+

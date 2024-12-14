e793102d0fa30a22560a3ef0f210537f8b3d2280
Blanca12luz <bl.hidalgo@duocuc.cl>
2024-12-13 23:31:21 -0300

    que otro usuario borre los viajes creados de otros conductores arreglado

--- src/app/pages/viajes/viajes.page.ts
+++ src/app/pages/viajes/viajes.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { firstValueFrom } from 'rxjs';
+import { AuthServiceService } from 'src/app/services/auth-service.service';
 
 @Component({
   selector: 'app-viajes',
@@ -8,33 +10,46 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 })
 export class ViajesPage implements OnInit {
   public viajes: any[] = []; // Lista de viajes obtenidos desde Firebase
+  user :any;
+  constructor(private firestore: AngularFirestore, private _auth:AuthServiceService) {}
 
-  constructor(private firestore: AngularFirestore) {}
-
-  ngOnInit() {
-    this.obtenerViajes(); // Llamamos al método para obtener los viajes al iniciar el componente
+  async ngOnInit() {
+    this.user = await this._auth.getUser()
+    await this.obtenerViajes(); // Llamamos al método para obtener los viajes al iniciar el componente
+    
   }
 
   /**
    * Método para obtener los viajes desde Firebase
    */
-  obtenerViajes() {
+
+  /**/
+  async obtenerViajes() {
+    
     this.firestore
       .collection('viajes') // Nombre de la colección en Firebase
       .valueChanges({ idField: 'id' }) // Recuperar datos con ID agregado como campo
       .subscribe(
         (data: any[]) => {
-          this.viajes = data; // Asignamos los datos obtenidos a la lista de viajes
+          this.viajes = data.filter ((doc:any)=>doc.usuario == this.user.uid); // Asignamos los datos obtenidos a la lista de viajes
           console.log('Viajes obtenidos:', this.viajes); // Mostramos los datos obtenidos en consola
         },
         (error) => {
           console.error('Error al obtener viajes:', error); // Capturamos cualquier error al obtener los viajes
         }
       );
+      
+    
+    
+    
+    
   }
 
   
+
+
   BorrarViaje(viajes: any) {
+    
     if (confirm(`¿Estás seguro de cancelar el viaje a ${viajes.id}`)) {
       this.firestore
         .collection('viajes')
